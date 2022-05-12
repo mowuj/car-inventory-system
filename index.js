8d6642b589290683e2b874774cadfd2c260dbdac
mowuj <mowuj92@gmail.com>
2022-05-12 19:25:23 +0600

    get car by id

--- index.js
+++ index.js
@@ -1,21 +1,39 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion,ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 require('dotenv').config();
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kkgin.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://InventoryDb:VuP8qgGzcedDmvTr@cluster0.kkgin.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-    console.log('inventory connected');
-  client.close();
-});
 
+async function run() {
+    
+    try {
+        await client.connect();
+        const carCollection = client.db('carInventory').collection('cars');
+        
+        app.get('/cars', async (req, res) => {
+            const query = {};
+            const cursor = carCollection.find(query);
+            const cars = await cursor.toArray();
+            res.send(cars)
+        })
+        app.get('/cars/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const car = await carCollection.findOne(query);
+            res.send(car);
+        });
+    }
+    finally {
+        
+    }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('running Car inventory Server');

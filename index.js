b90ec52b5cb0273c34e43c8688915d91b9892b3e
mowuj <mowuj92@gmail.com>
2022-05-12 21:50:34 +0600

    delete data

--- index.js
+++ index.js
@@ -28,6 +28,12 @@ async function run() {
             const car = await carCollection.findOne(query);
             res.send(car);
         });
+        app.delete('/cars/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await carCollection.deleteOne(query);
+            res.send(result);
+        });
     }
     finally {
         

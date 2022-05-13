bf584b98e7628863b86b0fdf70586fb44bc91d8d
mowuj <mowuj92@gmail.com>
2022-05-13 09:17:29 +0600

    jwt connent and login with jwt

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion,ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 require('dotenv').config();
 app.use(cors());
 app.use(express.json());
-
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({message:'unauthorized access'})
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({message:'Forbidden access'})
+        }
+        console.log('decoded', decoded);
+        req.decoded = decoded;
+        next();
+    })
+    
+    
+}
 const uri = `mongodb+srv://InventoryDb:VuP8qgGzcedDmvTr@cluster0.kkgin.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
@@ -15,7 +32,13 @@ async function run() {
     try {
         await client.connect();
         const carCollection = client.db('carInventory').collection('cars');
-        
+        app.post('/login', async (req, res) => {
+            const user = req.body;
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '1d'
+            });
+            res.send({ accessToken });
+        });
         app.get('/cars', async (req, res) => {
             const query = {};
             const cursor = carCollection.find(query);
@@ -33,20 +56,34 @@ async function run() {
             const result = await carCollection.insertOne(newCar);
             res.send(result);
         });
-        // app.put('/cars/:id', async (req, res) => {
-        //     const id = req.params.id;
-        //     const updatedQuantity = req.body;
-        //     const filter = { _id: ObjectId(id) };
-        //     const options = { upsert: true };
-        //     const updatedDoc = {
-        //         $set: {
-        //             quantity: updatedQuantity.quantity,
+        app.put('/cars/:id', async (req, res) => {
+            const id = req.params.id;
+            const updatedQuantity = req.body;
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updatedDoc = {
+                $set: {
+                    quantity: updatedQuantity.quantity
                     
-        //         }
-        //     };
-        //     const result = await userCollection.updateOne(filter, updatedDoc, options);
-        //     res.send(result)
-        // })  
+                }
+            };
+            const result = await carCollection.updateOne(filter, updatedDoc, options);
+            res.send(result)
+        })  
+        app.get('/cars', verifyJWT, async (req, res) => {
+            const decodedEmail = req.decoded.email
+            const email = req.query.email;
+            
+            if (email === decodedEmail) {
+                const query = { email: email };
+                const cursor = carCollection.find(query);
+                const car = await cursor.toArray();
+                res.send(car)
+            }
+            else {
+                res.status(403).send({ message: 'Forbidden access' })
+            }
+        });
         app.delete('/cars/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
